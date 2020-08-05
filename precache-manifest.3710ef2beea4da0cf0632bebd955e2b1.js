edec4d274a506b56f565857492ed246a80eb0834
Quardiz <quardiz@protonmail.com>
2020-08-05 16:40:38 +0200

    Updates

---
+++ precache-manifest.3710ef2beea4da0cf0632bebd955e2b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d056173b9d3bedaebecde967f5726bc2",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "1b863c77ae192bdf363c",
+    "url": "/Calculator/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "31483b167e53dee81cdc",
+    "url": "/Calculator/static/css/main.47cd368e.chunk.css"
+  },
+  {
+    "revision": "1b863c77ae192bdf363c",
+    "url": "/Calculator/static/js/2.b9db73cf.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Calculator/static/js/2.b9db73cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31483b167e53dee81cdc",
+    "url": "/Calculator/static/js/main.0f76cadc.chunk.js"
+  },
+  {
+    "revision": "0a79fa1d2af7875576c8",
+    "url": "/Calculator/static/js/runtime-main.8483c221.js"
+  }
+]);
